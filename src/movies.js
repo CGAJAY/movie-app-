270faf3a9b0a5ddc25ea264cf097d394037f5eb0
CGAJAY <jmugendi737@gmail.com>
2024-10-13 15:56:14 +0300

    Added a file with an array of my movies

---
+++ src/movies.js
@@ -0,0 +1,31 @@
+const movies = [
+	{
+		id: 1,
+		title: "Inception",
+		description:
+			"A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+		posterURL:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+		rating: 8.8,
+	},
+	{
+		id: 2,
+		title: "The Dark Knight",
+		description:
+			"When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
+		posterURL:
+			"https://m.media-amazon.com/images/S/pv-target-images/e9a43e647b2ca70e75a3c0af046c4dfdcd712380889779cbdc2c57d94ab63902.jpg",
+		rating: 9.0,
+	},
+	{
+		id: 3,
+		title: "Interstellar",
+		description:
+			"A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+		posterURL:
+			"https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_.jpg",
+		rating: 8.6,
+	},
+];
+
+export default movies;
