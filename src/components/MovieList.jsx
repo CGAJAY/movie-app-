4b3e54e450ff63d869ac9e4a9bda1113669adb43
CGAJAY <jmugendi737@gmail.com>
2024-10-13 15:40:55 +0300

    added a component to render the list of movies.

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,15 @@
+// src/components/MovieList.jsx
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ movies }) => {
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+			{movies.map((movie, index) => (
+				<MovieCard key={index} movie={movie} />
+			))}
+		</div>
+	);
+};
+
+export default MovieList;
