fce7bcc47f2382d5555ed0d617907d1f3843878f
CGAJAY <jmugendi737@gmail.com>
2024-11-13 17:36:53 +0300

    added id for every movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = ({ movies }) => {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 			{movies.map((movie, index) => (
-				<MovieCard key={index} movie={movie} />
+				<MovieCard key={index} id={index} movie={movie} />
 			))}
 		</div>
 	);
